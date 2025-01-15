876b7dc31e78db89d63a015fc618ccfa127eff42
avlnkov <alex.lenkov@mail.ru>
2025-01-15 12:55:04 +0300

    some fixes

--- app/src/main/java/com/example/couroutinestart/MainActivity.kt
+++ app/src/main/java/com/example/couroutinestart/MainActivity.kt
@@ -2,10 +2,7 @@ package com.example.couroutinestart
 
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
 import com.example.couroutinestart.databinding.ActivityMainBinding
 import kotlin.concurrent.thread
