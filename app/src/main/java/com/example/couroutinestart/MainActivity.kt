586c349baab6f324ce218b9bc175d4b6d7eb9d2b
avlnkov <alex.lenkov@mail.ru>
2025-01-15 12:54:10 +0300

    initial commit

---
+++ app/src/main/java/com/example/couroutinestart/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.couroutinestart
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import com.example.couroutinestart.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.buttonLoad.setOnClickListener {
+            loadData()
+        }
+    }
+
+
+    private fun loadData() {
+        binding.progress.isVisible = true
+        binding.buttonLoad.isEnabled = false
+
+        loadCity { city ->
+            binding.tvLocation.text = city
+            loadTemperature(city) {
+                loadTemperature(city) { temperature ->
+                    binding.tvTemperature.text = temperature.toString()
+                    binding.progress.isVisible = false
+                    binding.buttonLoad.isEnabled = true
+                }
+            }
+        }
+    }
+
+    private fun loadCity(callback: (String) -> Unit) {
+        thread {
+            val city = "Moscow"
+            Thread.sleep(5000)
+            callback.invoke(city)
+        }
+    }
+
+    private fun loadTemperature(city: String, callback: (Int) -> Unit) {
+        thread {
+            Toast.makeText(
+                this,
+                getString(R.string.loading_temperature_toast, city),
+                Toast.LENGTH_SHORT
+            ).show()
+            Thread.sleep(5000)
+            callback.invoke(17)
+        }
+    }
+}
